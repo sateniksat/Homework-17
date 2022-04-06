75ce9a7034cadf42a973977812a1a3169409bd6c
Satenik Shahi <shahijanisaten@gmail.com>
2022-04-06 18:05:12 +0430

    add

--- Satenik_Shahi_Jani_hw17_maktab66/my-app/src/App.js
+++ Satenik_Shahi_Jani_hw17_maktab66/my-app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import AllItems from './AllItems';
-import SelectedItems from './SelectedItems';
+import AllItems from './Components/AllItems';
+import SelectedItems from './Components/SelectedItems';
 
 
 // https://61f6a5402e1d7e0017fd6e5f.mockapi.io/names
